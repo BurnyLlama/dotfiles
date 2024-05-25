164a932f2998502d01aa8850d3978ea8c2919094
BurnyLlama <burnyllama@protonmail.com>
2024-05-25 16:09:00 +0200

    Theme added to dotfile repo.

---
+++ .config/zsh/theme.zsh
@@ -0,0 +1,48 @@
+#!/bin/zsh
+
+fpath=($fpath /data/zsh)
+
+# ZSH autoloads
+autoload -U  colors && colors
+autoload -Uz promptinit && promptinit
+autoload -Uz vcs_info
+
+# PROMPT modules
+local return_status="%(?:%F{green}✓:%F{red}✗)"
+local user_name="%F{green}%n"
+local host_name="%F{blue}%m"
+local user_indicator="%F{cyan}$"
+
+# Create the prompt
+format_prompt () {
+PROMPT="
+%B${user_name}%F{cyan}@${host_name} ${crnt_path}${vcs_info_msg_0_}
+${return_status} ${user_indicator}%f%b "
+}
+
+# Makes a path if in a normal directory and repo-name if in a vcs-folder.
+path_or_vcs () {
+    # Have it be a path or a repo name
+    [[ -z "$vcs_info_msg_0_" ]] && crnt_path="%F{red}󰉖 %/" || crnt_path=""
+
+    # If in git repo, give relative path
+    vcs_root_dir='%F{red}󰉖 %r %F{15}on %F{magenta}󰘬 %b'
+    vcs_sub_dir='%F{red}󰉖 %r/%S %F{15}on %F{magenta}󰘬 %b'
+
+    # This is the scuffed check :)
+    zstyle ':vcs_info:git*' formats '%S'
+    vcs_info
+    [[ "$vcs_info_msg_0_" == "." ]] \
+        && zstyle ':vcs_info:git*' formats $vcs_root_dir \
+        || zstyle ':vcs_info:git*' formats $vcs_sub_dir
+    
+    # Finally set the vcs_info again...
+    vcs_info
+}
+
+# Pre-cmd function
+precmd () {
+    vcs_info # Enable git status
+    path_or_vcs
+    format_prompt
+}
