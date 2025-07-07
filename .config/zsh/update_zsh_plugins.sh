3426ceb9f3ed7e79ebda503b2c2a6b66100aebb4
BurnyLlama <burnyllama@protonmail.com>
2025-07-07 02:22:08 +0200

    Added some safe guards to

--- .config/zsh/update_zsh_plugins.sh
+++ .config/zsh/update_zsh_plugins.sh
@@ -2,11 +2,11 @@
 
 # Add this to your crontab to keep it up to date automatically!
 
-cd "$ZDOTDIR"
+cd "$ZDOTDIR" || exit
 
 for PLUGIN in `find . -maxdepth 1 -mindepth 1 -type d`; do
     echo "Updating $PLUGIN ..."
-    cd $PLUGIN
+    cd "$PLUGIN" || return
     git pull
     cd ..
 done
