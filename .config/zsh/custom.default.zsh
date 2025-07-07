b9267a5b9349b8fe7d816bfdf0ebbcf113f6c9c4
BurnyLlama <burnyllama@protonmail.com>
2025-07-07 10:23:39 +0200

    Fixed default custom zsh config.

--- .config/zsh/custom.default.zsh
+++ .config/zsh/custom.default.zsh
@@ -1,3 +1,16 @@
 #!/bin/zsh
 
-source "$ZDOTDIR/custom.d/*.zsh"
+source $ZDOTDIR/custom.d/gentoo.zsh
+source $ZDOTDIR/custom.d/git.zsh
+source $ZDOTDIR/custom.d/dotfile.zsh
+source $ZDOTDIR/custom.d/edit.zsh
+source $ZDOTDIR/custom.d/kernel.zsh
+source $ZDOTDIR/custom.d/verbosity.zsh
+source $ZDOTDIR/custom.d/basics.zsh
+source $ZDOTDIR/custom.d/grc.zsh
+source $ZDOTDIR/custom.d/fzf.zsh
+source $ZDOTDIR/custom.d/arch.zsh
+source $ZDOTDIR/custom.d/void.zsh
+source $ZDOTDIR/custom.d/power_options.zsh
+source $ZDOTDIR/custom.d/rust.zsh
+source $ZDOTDIR/custom.d/node.zsh
